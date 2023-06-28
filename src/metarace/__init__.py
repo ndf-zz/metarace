5bdcb98152da1c33d2b2418c72925c4d3ab84acc
ndf-zz <ndf-zz@6-v.org>
2023-06-28 23:22:14 +1000

    Version 2.1.2

--- src/metarace/__init__.py
+++ src/metarace/__init__.py
@@ -13,7 +13,7 @@ try:
 except ImportError:
     print('Python >= 3.9 is required to use this module')
 
-VERSION = '2.1.1'
+VERSION = '2.1.2'
 DATA_PATH = os.path.realpath(
     os.path.expanduser(os.path.join('~', 'Documents', 'metarace')))
 DEFAULTS_PATH = os.path.join(DATA_PATH, 'default')
