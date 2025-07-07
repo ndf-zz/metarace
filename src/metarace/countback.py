e219b65d2d594fa5a8ef0b398274da0fc396cd5e
ndf-zz <ndf-zz@6-v.org>
2025-07-07 20:54:45 +1000

    Subtract and unary negate countback

--- src/metarace/countback.py
+++ src/metarace/countback.py
@@ -1,71 +1,103 @@
 # SPDX-License-Identifier: MIT
-"""Countback support."""
+"""Countback
+
+Track a countback of places for ordering points tallies.
+The zeroth place is reserved for stage races where 1st
+place on the stage finish is considered more significant
+than 1st place in intermediates.
+
+Example:
+
+    >>> a = countback()
+    >>> a[2] = 1  # 1 2nd place
+    >>> a[3] += 3  # 3 3rd places
+    >>> str(a)
+    '-,-,1,3'
+    >>> b = countback('-,1,-,1')
+    >>> b > a
+    True
+
+To sort a tally with countback, negate the countback
+and points:
+
+    st = []
+    for rno, pts, cb in something:
+        st.append((-pts, -cb, strops.bibstr_key(rno)))
+    st.sort()
+
+
+"""
 
 
 class countback:
-    """Wrapper for countback store/compare."""
+    """Dict wrapper for countback load/store/compare."""
     __hash__ = None
+
+    def maxplace(self):
+        """Return maximum place with non-zero count."""
+        ret = 0
+        if len(self.__store) > 0:
+            for k, v in self.__store.items():
+                if v != 0 and k > ret:
+                    ret = k
+        return ret
 
     def __init__(self, cbstr=None):
         self.__store = {}
         if cbstr is not None:
-            self.fromstring(cbstr)
+            self._fromstring(cbstr)
 
-    def maxplace(self):
-        """Return maximum non-zero place."""
-        ret = 0
-        if len(self.__store) > 0:
-            ret = max(self.__store.keys())
-        return ret
-
-    def fromstring(self, cbstr):
+    def _fromstring(self, cbstr):
+        """Re-populate store with counts from provided cb string."""
         propmap = {}
         cbvec = cbstr.split(',')
         if len(cbvec) > 0:
-            for i in range(0, len(cbvec)):
-                if cbvec[i].isdigit():
-                    propmap[i] = int(cbvec[i])
+            for i, v in enumerate(cbvec):
+                v = v.strip()
+                if v and v != '-':
+                    propmap[i] = int(v)
+        # update store after entire string has been processed
         self.__store = {}
-        for k in propmap:
-            self.__store[k] = propmap[k]
+        for k, v in propmap.items():
+            self.__store[k] = v
+
+    def items(self):
+        return self.__store.items()
+
+    def keys(self):
+        return self.__store.keys()
 
     def __repr__(self):
-        return 'countback({})'.format(repr(self.__unicode__()))
+        return 'countback({})'.format(repr(str(self)))
 
     def __str__(self):
-        return str(self.__unicode__())
-
-    def __unicode__(self):
         ret = []
         for i in range(0, self.maxplace() + 1):
-            if i in self.__store and self.__store[i] != 0:
-                ret.append(str(self.__store[i]))
+            v = self[i]
+            if v != 0:
+                ret.append(str(v))
             else:
                 ret.append('-')
         return ','.join(ret)
 
-    def __len__(self):
-        return len(self.__store.len)
-
     def __getitem__(self, key):
+        # Note: unlike defaultdict, a missing key should not be
+        #       added to store until it has a non-default value
         if key in self.__store:
             return self.__store[key]
         else:
             return 0
 
     def __setitem__(self, key, value):
-        if isinstance(key, int):
+        if isinstance(key, int) and key >= 0:
             self.__store[key] = value
         else:
-            raise AttributeError('Countback keys must be integer')
+            raise AttributeError('Countback keys must be integer >= 0')
 
     def __delitem__(self, key):
         del (self.__store[key])
 
     def __iter__(self):
-        return iter(self.__store.keys())
-
-    def iterkeys(self):
         return iter(self.__store.keys())
 
     def __contains__(self, item):
@@ -140,10 +172,29 @@ class countback:
         return ret
 
     def __add__(self, other):
-        """Add two countbacks together and return a new cb>=self >=other."""
         if not isinstance(other, countback):
             return NotImplemented
-        ret = countback(str(self))
+        ret = countback()
         for i in range(0, max(self.maxplace(), other.maxplace()) + 1):
-            ret[i] += other[i]
+            v = self[i] + other[i]
+            if v != 0:
+                ret[i] = v
+        return ret
+
+    def __sub__(self, other):
+        if not isinstance(other, countback):
+            return NotImplemented
+        ret = countback()
+        for i in range(0, max(self.maxplace(), other.maxplace()) + 1):
+            v = self[i] - other[i]
+            if v != 0:
+                ret[i] = v
+        return ret
+
+    def __neg__(self):
+        ret = countback()
+        for i in range(0, self.maxplace() + 1):
+            v = self[i]
+            if v != 0:
+                ret[i] = -v
         return ret
